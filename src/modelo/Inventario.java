4ec8a03815dad8b0532b28850089aa9457c8d05f
Halley <jarod.menatutiven@gmail.com>
2026-03-18 14:02:34 +0100

    Cambios 2.0

--- src/modelo/Inventario.java
+++ src/modelo/Inventario.java
@@ -8,11 +8,46 @@ public class Inventario {
     private int capacidadMaxima;
     private ArrayList<String> categorias;
     
+
+    //Constructor
     public Inventario (int totalProductos, int capacidadMaxima) {
         
     this.totalProductos = totalProductos;
     this.capacidadMaxima = capacidadMaxima;
-
         
+        System.out.println("BLYATTTTTTTTT");
+
     }
+
+
+    public int getTotalProductos() {
+        return totalProductos;
+    }
+
+
+    public void setTotalProductos(int totalProductos) {
+        this.totalProductos = totalProductos;
+    }
+
+
+    public int getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
+
+
+    public void setCapacidadMaxima(int capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+    }
+
+
+    public ArrayList<String> getCategorias() {
+        return categorias;
+    }
+
+
+    public void setCategorias(ArrayList<String> categorias) {
+        this.categorias = categorias;
+    }
+
+
 }
