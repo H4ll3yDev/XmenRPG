b022b69737f6d82ee65c582db893858b235d3d68
Halley <jarod.menatutiven@gmail.com>
2026-03-18 12:59:28 +0100

    cambios en inventarios

--- src/modelo/Inventario.java
+++ src/modelo/Inventario.java
@@ -1,5 +1,18 @@
 package modelo;
 
+import java.util.ArrayList;
+
 public class Inventario {
+
+    private int totalProductos;
+    private int capacidadMaxima;
+    private ArrayList<String> categorias;
     
+    public Inventario (int totalProductos, int capacidadMaxima) {
+        
+    this.totalProductos = totalProductos;
+    this.capacidadMaxima = capacidadMaxima;
+
+        
+    }
 }
