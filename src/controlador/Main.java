1eff88e1fbbda1b21f7c89318a57cce1ebfb381c
Marcos <torregrosamarcos27@gmail.com>
2026-03-24 09:01:52 +0100

    a

--- src/controlador/Main.java
+++ src/controlador/Main.java
@@ -6,6 +6,7 @@ import modelo.sistema.*;
 
 public class Main {
     public static void main(String[] args) {
+    	
         A_Lobezno lobezno = new A_Lobezno();
         E_Magneto magneto = new E_Magneto();
         Combate combate = new Combate(lobezno, magneto);
